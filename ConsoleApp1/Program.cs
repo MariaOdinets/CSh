36b961188f4f323b85dbc60d2db7d080b9d0c876
Mary Odinets <mariaodinets@gmail.com>
2023-02-20 23:50:28 +0300

    Changed output string text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name);
+            Console.WriteLine("Hell, " + name);
         }
     }
 }
