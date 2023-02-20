0d0aaf299c09f876671ee1fa402c7913784cd828
Mary Odinets <mariaodinets@gmail.com>
2023-02-21 00:21:57 +0300

    added "Hello"

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             string name = Console.ReadLine();
-            Console.WriteLine("Hell, " + name);
+            Console.WriteLine("Hello, " + name);
         }
     }
 }
