fb8987d9dc7e946f653a9b8515535073fdd1095e
Mary Odinets <mariaodinets@gmail.com>
2023-02-23 23:53:04 +0300

    finished adding all exampleas from lecture

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,23 +4,64 @@
     {
         static void Main(string[] args)
         {
-           // Task1();
-            Task2();
+            //Task1();
+            //Task2();
+            Task3();
+            //Task4();
         }
 
 
+        public static void Task4()
+        {
+            int number = int.Parse(Console.ReadLine());
+            double result;
+
+            if (number < 100)
+            {
+                result = number * 0.05;
+            }
+            else if ((number >= 100) && (number <= 200))
+            {
+                result = number * 0.07;
+            }
+            else
+            {
+                result = number * 0.1;
+            }
+            Console.WriteLine($"Your % plus deposite is {result}");
+
+        }
+
+
+
+        public static void Task3()
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            if ((input > 5) || (input < 10))
+            {
+                Console.WriteLine("Число больше 5 и меньше 10");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число");
+            }
+
+        }
+
         public static void Task2()
         {
-            int numberOne = 1; 
+            int numberOne = 1;
             int numberTwo = 2;
 
-            if (numberOne > numberTwo) 
+            if (numberOne > numberTwo)
             {
-                Console.WriteLine("First number is bigger than second");
-            } else
+                Console.WriteLine("First number is bigger than second one");
+            }
+            else
             {
                 {
-                    Console.WriteLine("Second number is bigger than first one");
+                    Console.WriteLine("Second number is bigger than the first one");
                 }
             }
         }
