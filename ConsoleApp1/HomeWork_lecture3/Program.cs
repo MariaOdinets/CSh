d50dffdd6680f84e53553ac58c7ca0839d23d082
Mary Odinets <mariaodinets@gmail.com>
2023-03-06 22:02:29 +0300

    Completed task 1

--- ConsoleApp1/HomeWork_lecture3/Program.cs
+++ ConsoleApp1/HomeWork_lecture3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ConsoleApp1.HomeWork;
 using ConsoleApp1.HomeWork_lecture5;
+using System.Xml.Linq;
 
 namespace ConsoleApp1.HomeWork1
 {
@@ -7,15 +8,18 @@ namespace ConsoleApp1.HomeWork1
     {
         static void Main()
         {
-            Phone mashasPhone = new Phone(15456465, "Iphone X", 0.144, "Гриша");
+            Phone mashasPhone = new Phone(15456465, "Iphone X", 0.144);
 
             Phone mishasPhone = new Phone(24564464, "Samsung s22");
 
             Phone grishasPhone = new Phone();
 
             //grishasPhone.GetInfo();
-            //mashasPhone.ReceiveCall();
-            grishasPhone.GetNumber();
+            //mashasPhone.ReceiveCall("Mike");
+            //mashasPhone.ReceiveCall(12356114, "Мила");
+            mashasPhone.SendMessage(13545, 54545465, 46545);
+            // mishasPhone.GetNumber();
+
         }
     }
 }
